7cb2d5ee07cf4f85a9a8851495d88a555edcc250
tomczak-mateusz <mateusz.tomczak@sperasoft.com>
2023-01-16 15:37:29 +0100

    [1] initialize Cosmopolitan::PeselValidator gem - include Ruby 3.1.2, RSpec, GitHub Actions

---
+++ lib/cosmopolitan/pesel_validator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "pesel_validator/version"
+
+module Cosmopolitan
+  module PeselValidator
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/cosmopolitan/pesel_validator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cosmopolitan::PeselValidator do
+  it "has a version number" do
+    expect(Cosmopolitan::PeselValidator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
