aa40bf45f5f3baa37e4d15a24b0e3631fa1ee072
Mateusz Tomczak <99652987+tomczak-mateusz@users.noreply.github.com>
2023-01-17 19:23:16 +0100

    [9] add random order to RSpec config (#8)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,9 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
+  # Random spec order
+  config.order = :random
+
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
