c7493d50cf4e05fa730019633f509c538f4c7344
Mateusz Tomczak <99652987+tomczak-mateusz@users.noreply.github.com>
2023-01-17 19:06:11 +0100

    [3] remove initial failing spec - 'does something useful' (#2)

--- spec/cosmopolitan/pesel_validator_spec.rb
+++ spec/cosmopolitan/pesel_validator_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Cosmopolitan::PeselValidator do
   it "has a version number" do
     expect(Cosmopolitan::PeselValidator::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
