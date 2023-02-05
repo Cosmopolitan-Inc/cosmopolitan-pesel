d881b999235e15e4c491c1c8333ed296f51a074c
tomczak-mateusz <mateusz.tomczak@sperasoft.com>
2023-02-05 12:28:46 +0100

    [28] define gender? method, Gender class & specs

--- lib/cosmopolitan/pesel_validator.rb
+++ lib/cosmopolitan/pesel_validator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'pesel_validator/check_digit'
+require_relative 'pesel_validator/gender'
 require_relative 'pesel_validator/validate_value_format'
 require_relative 'pesel_validator/version'
 
@@ -9,6 +10,11 @@ module Cosmopolitan
     def self.check_digit?(value)
       ValidateValueFormat.new(value).call
       CheckDigit.new(value).call
+    end
+
+    def self.gender?(value)
+      ValidateValueFormat.new(value).call
+      Gender.new(value).call
     end
   end
 end
