e9b15115665d5b96774d39e54c1f68669c9a4f3b
tomczak-mateusz <mateusz.tomczak@sperasoft.com>
2023-02-05 11:45:26 +0100

    [27] define ValidateValueFormat & InvalidValueError classes and specs, update check_digit? method

--- lib/cosmopolitan/pesel_validator.rb
+++ lib/cosmopolitan/pesel_validator.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 require_relative 'pesel_validator/check_digit'
+require_relative 'pesel_validator/validate_value_format'
 require_relative 'pesel_validator/version'
 
 module Cosmopolitan
   module PeselValidator
     def self.check_digit?(value)
+      ValidateValueFormat.new(value).call
       CheckDigit.new(value).call
     end
   end
